75b3597e442d74ca774a49507c22161e9d6bdea0
Subrota Debnath <subrotadebnath1807@gmail.com>
2021-06-22 21:33:01 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "418155dd91aa0d3452f01b062ff188f9",
-"/": "418155dd91aa0d3452f01b062ff188f9",
-"main.dart.js": "c4f760b27e8c2e9d6babaff2b31ede0d",
+"index.html": "2cff4b1b163f8d290624e4f2ffd4f109",
+"/": "2cff4b1b163f8d290624e4f2ffd4f109",
+"main.dart.js": "8cdd8bb4a6ffc1d019917075990db399",
 "manifest.json": "95ccface639a8cbd152cb1af8479545c",
 "version.json": "859a0dba6334b9dd1c900214474e2304"
 };
